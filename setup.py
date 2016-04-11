86c830c2eb0243a545cb628aa7825f11cd6aa2b4
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-04-11 15:40:12 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,32 +6,29 @@
 
 from setuptools import setup
 
-
-setup(
-    name="sentaku",
-    use_scm_version=True,
-    author="Milan Falešník",
-    author_email="milan@falesnik.net",
-    description="Python deprecating solution.",
-    license="GPLv3",
-    keywords="testing",
-    url="https://github.com/RonnyPfannschmidt/Sentaku",
-    packages=["sentaku"],
-    package_dir={'': 'src'},
-    install_requires=[
-        'attrs',
-    ],
-    setup_requires=[
-        'setuptools_scm',
-    ],
-    classifiers=[
-        "Topic :: Utilities",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Intended Audience :: Developers",
-        "Development Status :: 4 - Beta",
-    ]
-)
+setup(name="sentaku",
+      use_scm_version=True,
+      author="RonnyPfannschmidt",
+      author_email="opensource@ronnypfannschmidt.de",
+      description="variadic ux implemntation for testing",
+      license="GPLv3",
+      keywords=["testing"],
+      url="https://github.com/RonnyPfannschmidt/Sentaku",
+      packages=["sentaku"],
+      package_dir={'': 'src'},
+      install_requires=[
+          'attrs',
+      ],
+      setup_requires=[
+          'setuptools_scm',
+      ],
+      classifiers=[
+          "Topic :: Utilities",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+      ])
