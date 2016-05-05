5400bd4a942c7a33ae593785000ca71cf21f3dc3
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-05-05 14:03:30 +0200

    fix flake8 issue

--- testing/test_implstack.py
+++ testing/test_implstack.py
@@ -16,6 +16,7 @@ def test_nonempty(chooser):
     with chooser.pushed(1):
         assert chooser.current == 1
 
+
 def test_freezing(chooser):
     with chooser.pushed(1, frozen=True):
         with pytest.raises(RuntimeError):
