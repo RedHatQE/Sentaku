2c6caf60f049d226a9ad37fc8c4d17f326a46055
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-05-09 09:09:38 +0200

    remove unused code

--- src/sentaku/utils.py
+++
@@ -1,9 +0,0 @@
-from operator import attrgetter
-
-
-def alias(name):
-    """creates an alias to a dotted attribute"""
-
-    doc = "alias for self.{name}".format(name=name)
-    fget = attrgetter(name)
-    return property(fget=fget, doc=doc)
