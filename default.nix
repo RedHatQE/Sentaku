06b6535e4e8593f820224ac431828c2c543b6303
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-06-14 22:15:22 +0200

    add forgotten files

---
+++ default.nix
@@ -0,0 +1,58 @@
+with import <nixpkgs> {};
+with pkgs;
+with python27Packages;
+stdenv.mkDerivation rec {
+  name = "sentaku-test";
+  src = null;
+  buildInputs = [
+    gitFull
+    less
+    ncurses
+    openssh
+    bpython
+    sqlite
+    setuptools_scm
+    pytest
+    flake8
+    pygments
+    sphinx
+    requests
+    selenium
+    virtualenv
+    watchdog
+  ];
+
+  shellHook=''
+  export PS1="(${name}) \w \\$ \[$(tput sgr0)\]"
+  export PYTHONPATH="$PYTHONPATH:./src"
+  test() {
+    py.test
+  }
+
+  build_docs() {
+    sphinx-build -b html -d build/sphinx-doctrees docs build/htmldocs
+  }
+
+  autobuild_docs() {
+    watchmedo shell-command docs src -R \
+      -c 'sphinx-build -b html -d build/sphinx-doctrees docs build/htmldocs'
+  }
+
+  clean() {
+    find -name \*.pyc -delete
+    find -name __pycache__ -delete
+    rm -rf build/sphinx-doctrees build/htmldocs
+  }
+
+  build_fresh_docs() {
+    clean
+    build_docs
+  }
+
+  test_clean() {
+    clean
+    test
+  }
+
+  '';
+}
---
+++ docs/getting_started.rst
@@ -0,0 +1,31 @@
+quickly combining things
+========================
+
+the getting started guide is based on the :ref:`mini-example`
+
+
+0. importing all the things we need in the example
+
+  .. literalinclude:: ../examples/mini_example.py
+    :lines: 10-20
+
+1. identifying the implementations you want to be able to use
+   and giving them a name.
+
+  .. literalinclude:: ../examples/mini_example.py
+    :lines: 21-29
+
+2. Declaring a element class and some contextual methods
+
+  .. literalinclude:: ../examples/mini_example.py
+    :lines: 31-33, 55
+
+3. registration of the contextual method implementations
+
+  .. literalinclude:: ../examples/mini_example.py
+    :lines: 34-54, 57-60
+
+4. bringing it all together
+
+  .. literalinclude:: ../examples/mini_example.py
+    :lines: 10-20, 62-
