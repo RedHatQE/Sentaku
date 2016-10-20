3f06784117e3e464b5ed1ba1772a012360af1023
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-10-20 10:44:25 +0200

    finish generalizing basic registration

--- src/sentaku/context.py
+++ src/sentaku/context.py
@@ -1,4 +1,5 @@
 import contextlib
+METHOD_DATA_KEY = 'sentaku_method_data'
 
 
 class ImplementationContext(object):
@@ -106,15 +107,20 @@ class _KeyRegistry(dict):
             self[key] = val
 
 
-
 def function_registring_decorator(registry, keys, result):
     def register_and_return(func):
         assert not isinstance(func, type(result))
         registry.add_implementations(keys, func)
         return result
     return register_and_return
-        
 
+
+def function_marking_decorator(registry, keys):
+    def mark_function(func):
+        method_data = func.__dict__.setdefault(METHOD_DATA_KEY, [])
+        method_data.append((registry, keys))
+        return func
+    return mark_function
 
 
 class ContextualMethod(object):
@@ -143,24 +149,14 @@ class ContextualMethod(object):
         return '<ContextualMethod {implementations}>'.format(
             implementations=sorted(self.implementations.keys()))
 
-    def _add_implementations(self, keys, func):
-        for key in keys:
-            assert key not in self.implementations
-            self.implementations[key] = func
-
     def implemented_for(self, *implementations):
         """
         decorator that registers a new implementation and returns the descriptor
         """
         return function_registring_decorator(self.implementations, implementations, self)
-        
 
     def external_implementation_for(self, *implementations):
-        def marking_decroator(func):
-            assert _get_method_data(func) is None
-            func._sentaku_method_data = (implementations, self)
-            return func
-        return marking_decroator
+        return function_marking_decorator(self.implementations, implementations)
 
     def __get__(self, instance, *_ignored):
         if instance is None:
@@ -170,11 +166,9 @@ class ContextualMethod(object):
 
 class ContextualProperty(object):
 
-
     def __init__(self):
         self.setters = _KeyRegistry()
         self.getters = _KeyRegistry()
-
 
     def setter_implemented_for(self, *implementations):
         """
@@ -188,10 +182,11 @@ class ContextualProperty(object):
         """
         return function_registring_decorator(self.getters, implementations, self)
 
+    def external_setter_implemented_for(self, *implementations):
+        return function_marking_decorator(self.setters, implementations)
 
-    #evil hack
-    external_setter_implemented_for = setter_implemented_for
-    external_getter_implemented_for = getter_implemented_for
+    def external_getter_implemented_for(self, *implementations):
+        return function_marking_decorator(self.getters, implementations)
 
     def __set__(self, instance, value):
 
@@ -217,22 +212,17 @@ class ContextualProperty(object):
 
 
 def _get_method_data(func):
-    return getattr(func, '_sentaku_method_data', None)
+    return getattr(func, METHOD_DATA_KEY, [])
 
 
 def _iter_metadata(module):
-    for val in vars(module).values():
-        data = _get_method_data(val)
-        if data:
-            try:
-                keys, method = data
-            except (ValueError, TypeError):
-                pass
-            else:
-                yield val, keys, method
+    for maybe_func in vars(module).values():
+
+        for registry, keys in _get_method_data(maybe_func):
+            yield maybe_func, keys, registry
 
 
 def register_external_implementations_in(*modules):
     for module in modules:
-        for func, keys, method in _iter_metadata(module):
-            method._add_implementations(keys, func)
+        for func, keys, registry in _iter_metadata(module):
+            registry.add_implementations(keys, func)
