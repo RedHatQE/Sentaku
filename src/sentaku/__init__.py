205fc2ac9e2baac68bd7a8532480e602e78d40a6
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-04-18 18:17:59 +0200

    temporary restore register_external_implementations_in

--- src/sentaku/__init__.py
+++ src/sentaku/__init__.py
@@ -1,4 +1,4 @@
-
+import warnings
 
 from .utils import (
     ImplementationName,
@@ -10,6 +10,19 @@ from .context import (
     ContextualMethod, ContextualProperty,
     ImplementationContext,
 )
+
+
+def register_external_implementations_in(*modules):
+    warnings.warn(
+        message="register_external_implementations_in is deprecated,"
+                " please use importscan.scan",
+        category=DeprecationWarning,
+        stacklevel=2,
+    )
+    import importscan
+    for module in modules:
+        importscan.scan(module)
+
 
 __all__ = [
     'Element',
