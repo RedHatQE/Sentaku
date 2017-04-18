5dc795491bdaadca15c380317568785e0e7105ed
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-04-18 19:46:28 +0200

    split ElementMixin out of Element

--- src/sentaku/modeling.py
+++ src/sentaku/modeling.py
@@ -1,18 +1,7 @@
 import attr
 
 
-@attr.s
-class Element(object):
-    """Base class for all application elements
-
-    :param parent:
-        controlling object of the element which is either a context
-        or a surrounding element
-    :type parent: :py:class:`Element` or :py:class:`ImplementationContext`
-    """
-
-    parent = attr.ib(repr=False)
-
+class ElementMixin(object):
     @property
     def context(self):
         """the context this element belongs to"""
@@ -22,6 +11,19 @@ class Element(object):
     def impl(self):
         """shortcut to get the currently active application implementation"""
         return self.context.impl
+
+
+@attr.s
+class Element(ElementMixin):
+    """Base class for all application elements
+
+    :param parent:
+        controlling object of the element which is either a context
+        or a surrounding element
+    :type parent: :py:class:`Element` or :py:class:`ImplementationContext`
+    """
+
+    parent = attr.ib(repr=False)
 
 
 @attr.s
