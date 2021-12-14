5b40292f8e9be3ac7a02d935154ab11b9f06efb5
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2021-12-14 10:06:37 +0100

    drop register_external_implementations_in as importscan was suggested

--- src/sentaku/__init__.py
+++ src/sentaku/__init__.py
@@ -1,22 +1,7 @@
-import warnings
-
 from .utils import ImplementationName, AttributeBasedImplementations
 
 from .modeling import Element, Collection
 from .context import ContextualMethod, ContextualProperty, ImplementationContext
-
-
-def register_external_implementations_in(*modules):
-    warnings.warn(
-        message="register_external_implementations_in is deprecated,"
-        " please use importscan.scan",
-        category=DeprecationWarning,
-        stacklevel=2,
-    )
-    import importscan
-
-    for module in modules:
-        importscan.scan(module)
 
 
 __all__ = [
