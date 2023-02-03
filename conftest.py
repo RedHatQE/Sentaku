07d10f46aefe3e4ac897450bc60053281391f64c
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2023-02-03 11:11:53 +0100

    update workflows

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-import sys
-
-
-def pytest_configure():
-    me = Path(__file__)
-    examples = me.parent / "examples"
-    sys.path.insert(0, os.fspath(examples))
